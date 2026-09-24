8a2b8a7de20796c689bde1757c83826d0384bbb0
enocla <tnixxc@gmail.com>
2026-09-23 21:12:04 -0400

    crimson

--- modules/base/fonts.nix
+++ modules/base/fonts.nix
@@ -2,6 +2,7 @@
   customPkgs = import ../../pkgs {inherit pkgs;};
 in {
   fonts.packages = [
+    pkgs.crimson-pro
     customPkgs."berkeley-mono-nerd-font"
     customPkgs."sf-pro-text"
     pkgs.nerd-fonts.symbols-only
